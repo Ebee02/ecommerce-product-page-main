e4a9ae6ecf0ffcaf0c63e99a615bda6f9f8fb045
Ebee02 <beelawrence8@gmail.com>
2024-07-19 20:38:20 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Header from "./components/Header/Header";
-import MainSlider from "./components/Slider/Slider";
-import Cart from "./components/Cart/Cart";
-import Button from "./components/Button/Button";
-import React from "react";
-import { useState } from "react";
+import Header from './components/Header';
+import MainSlider from './components/Slider';
+import Cart from './components/Cart';
+import Button from './components/Button';
+import React from 'react';
+import { useState } from 'react';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
@@ -20,37 +20,38 @@ const App = () => {
 
   const addToCart = () => {
     setCardItem((prevState) => prevState + counter);
+    setCounter(0);
   };
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <div className="main-container w-[375px] h-[810px] shadow-lg my-6">
+    <main className='min-h-screen bg-slate-300 flex items-center justify-center'>
+      <div className='main-container bg-white w-[390px] h-[825px] shadow-lg my-6 rounded-sm'>
         <Header cartItem={cartItem} />
         <MainSlider />
-        <div className="px-6">
-          <h3 className="text-main-orange text-base my-5 font-bold tracking-wider">
+        <div className='px-6'>
+          <h3 className='text-main-orange text-base my-5 font-bold tracking-wider'>
             SNEAKER COMPANY
           </h3>
-          <h1 className="text-3xl font-bold text-super-dark-blue mb-5">
+          <h1 className='text-3xl font-bold text-super-dark-blue mb-5'>
             Fall Limited Edition Sneakers
           </h1>
-          <p className="text-sm text-dark-grayish-blue tracking-wide font-semibold leading-7 mb-6">
+          <p className='text-sm text-dark-grayish-blue tracking-wide font-semibold leading-7 mb-6'>
             These low-profile sneakers are your perfect casual wear companion.
             Featuring a durable rubber outer sole, they’ll withstand everything
             the weather can offer.
           </p>
 
-          <div className="stats flex items-center justify-between font-bold text-super-dark-blue">
-            <div className="flex items-center gap-x-6">
-              <h2 className="text-3xl ">$125.00</h2>
-              <div className="bg-pale-orange text-main-orange px-2  rounded-sm shadow-sm">
+          <div className='stats flex items-center justify-between font-bold text-super-dark-blue'>
+            <div className='flex items-center gap-x-6'>
+              <h2 className='text-3xl '>$125.00</h2>
+              <div className='bg-pale-orange text-main-orange px-2  rounded-sm shadow-sm'>
                 50%
               </div>
             </div>
-            <h3 className="text-xl line-through text-dark-grayish-blue">
-              $250.00{" "}
+            <h3 className='text-xl line-through text-dark-grayish-blue'>
+              $250.00{' '}
             </h3>
           </div>
-          <div className="mt-6">
+          <div className='mt-6'>
             <Cart
               count={counter}
               onHandleIncrementCounter={incrementCounter}
---
+++ src/components/Slider.jsx
@@ -0,0 +1,44 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+// import textPhoto from '../../../public/images/mobile-design.jpg';
+
+const MainSlider = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  const sliderElement = sliderData.map((data) => {
+    return <img key={data.id} className='w-full h-64' src={data.img}></img>;
+  });
+  return (
+    <Slider {...settings} className='w-full h-64 mt-1'>
+      {sliderElement}
+    </Slider>
+  );
+};
+
+const sliderData = [
+  {
+    id: 1,
+    img: 'images/image-product-1.jpg',
+  },
+  {
+    id: 2,
+    img: 'images/image-product-2.jpg',
+  },
+  {
+    id: 3,
+    img: 'images/image-product-3.jpg',
+  },
+  {
+    id: 4,
+    img: 'images/image-product-4.jpg',
+  },
+];
+
+export default MainSlider;
